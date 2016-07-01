bb637a0026a3c4cc7bc8777e1f9d66ec5601ade4
Patrick Muff <muff.patrick@muffag.ch>
2016-07-01 13:35:38 +0200

    Use ExtractTextPlugin to create a compiled style.css file

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 var webpack = require('webpack');
 var path = require('path');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 var BUILD_DIR = path.resolve(__dirname, 'src/client/public');
 var APP_DIR = path.resolve(__dirname, 'src/client/app');
@@ -19,12 +20,12 @@ var config = {
       },
       {
         test: /\.scss$/,
-        loaders: ['style', 'css', 'sass']
+        loader: ExtractTextPlugin.extract(['css', 'sass'])
       }
     ]
   },
   plugins: [
-    //new webpack.optimize.UglifyJsPlugin()
+    new ExtractTextPlugin('styles.css')
   ]
 };
 
